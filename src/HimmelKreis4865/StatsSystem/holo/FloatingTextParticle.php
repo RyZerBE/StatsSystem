de042a24aa480f6ea87bd3ed6a4365f78900cf8a
Matze <47496465+Matze997@users.noreply.github.com>
2021-10-16 13:59:23 +0200

    We first have to remove the old code

--- src/HimmelKreis4865/StatsSystem/holo/FloatingTextParticle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HimmelKreis4865\StatsSystem\holo;
-
-class FloatingTextParticle extends \pocketmine\level\particle\FloatingTextParticle {
-	
-	/** @var int $entityId hack for making $entityId accessible */
-	public $entityId;
-}
