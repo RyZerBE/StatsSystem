ce122dee67ce1dbcdfd31eef60fe708c1fb50fa3
Matze <47496465+Matze997@users.noreply.github.com>
2021-12-11 22:21:53 +0100

    Fix, I hope

--- src/ryzerbe/statssystem/hologram/HologramUpdateTask.php
+++ src/ryzerbe/statssystem/hologram/HologramUpdateTask.php
@@ -7,7 +7,7 @@ use pocketmine\scheduler\Task;
 class HologramUpdateTask extends Task {
     public function onRun(int $currentTick){
         foreach(StatsHologramManager::getInstance()->activeStatsHolograms as $hologram) {
-            if($currentTick % (20 * 15) !== 0 || !$hologram->needUpdate) continue;
+            if(($currentTick % (20 * 15) !== 0 || !$hologram->needUpdate) && ($currentTick % (20 * 45) !== 0)) continue;
             $hologram->onUpdate();
         }
     }
